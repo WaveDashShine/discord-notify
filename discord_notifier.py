7c6bd998dfbc5bf03e01a345249dfc78e1878d0d
Erik Cui <erikcui9020@gmail.com>
2025-04-07 13:33:44 -0700

    add handling for playwright timeout error

--- discord_notifier.py
+++ discord_notifier.py
@@ -2,6 +2,7 @@ import time
 import random
 import asyncio
 import discord
+from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
 from configs import DISCORD_TOKEN, DISCORD_CHANNEL_ID
 from debug.debug_log import logger
 from manhwa_checker.main import get_manhwa_updates
@@ -39,14 +40,17 @@ def main():
     locked_chapters: list[Chapter] = []
     while True:
         logger.info(f"Checking at {datetime.now()}")
-        chapter_list: list[Chapter] = get_manhwa_updates(
-            timer=timer, locked_chapters=locked_chapters
-        )
-        if chapter_list:
-            logger.info(f"New chapters: {chapter_list}")
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(send_message(chapter_list))
-        timer = Timer()
+        try:
+            chapter_list: list[Chapter] = get_manhwa_updates(
+                timer=timer, locked_chapters=locked_chapters
+            )
+            if chapter_list:
+                logger.info(f"New chapters: {chapter_list}")
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(send_message(chapter_list))
+                timer = Timer()
+        except PlaywrightTimeoutError as e:
+            logger.error(e)
         polling_rate_seconds = 60 * POLLING_RATE
         time_to_sleep: int = random.randrange(
             start=int(polling_rate_seconds * 0.9), stop=int(polling_rate_seconds * 1.1)
