ace64a71acc4c17e8e24667fc28473e8476426cb
Erik Cui <erikcui9020@gmail.com>
2025-04-06 21:48:46 -0700

    forward + rewind tests

--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,4 +1,5 @@
-from datetime import timedelta
+import datetime
+from datetime import timedelta, datetime
 from manhwa_checker.timer import Timer
 
 
@@ -18,3 +19,15 @@ class TestTimer:
     def test_newly_available_days(self):
         timer = Timer(rewind=timedelta(hours=25))
         assert timer.is_new_since_start_time(time_string="1 day ago")
+
+    def test_rewind(self):
+        delta = timedelta(seconds=60)
+        timer = Timer(rewind=delta)
+        assert timer.start_time > datetime.now() - timedelta(seconds=61)
+        assert timer.start_time < datetime.now() - timedelta(seconds=59)
+
+    def test_forward(self):
+        delta = timedelta(seconds=60)
+        timer = Timer(forward=delta)
+        assert timer.start_time < datetime.now() + timedelta(seconds=61)
+        assert timer.start_time > datetime.now() + timedelta(seconds=59)
